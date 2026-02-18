cfeaeebb279bd457bd1d1a277c88459e04884978
Paramprakash Makwana <prakashmakwana7808@gmail.com>
2026-02-18 15:44:55 +0530

    commit

--- Project/Django Project/FirstProj/FirstProj/urls.py
+++ Project/Django Project/FirstProj/FirstProj/urls.py
@@ -20,5 +20,7 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
+    path('index/',views.index),
+    path('about/',views.about),
+    path('contact/',views.contact),
 ]
