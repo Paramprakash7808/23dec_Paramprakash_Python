27bcb8585bb0f7d93be7cc65bd517c48382c7232
Paramprakash Makwana <prakashmakwana7808@gmail.com>
2026-03-21 16:19:10 +0530

    commit

---
+++ Project/Django Project/TemplateInheritance/myapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.base),
+    path('base/', views.base),
+    path('home/', views.home),
+    path('about/', views.about),
+    path('contact/', views.contact)
+]
