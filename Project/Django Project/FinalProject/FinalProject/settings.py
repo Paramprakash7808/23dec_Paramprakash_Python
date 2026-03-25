68a9a83d82c2b29d6a8c6b72b1ac3ff21ecf8a54
Paramprakash Makwana <prakashmakwana7808@gmail.com>
2026-03-25 18:06:38 +0530

    commit

--- Project/Django Project/FinalProject/FinalProject/settings.py
+++ Project/Django Project/FinalProject/FinalProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-txmtmypo&sp(#gb7g(085x6i6yvcomr2q+hlw-2dv&jut3d%cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
