30145138bff59ed76cfc5f6141d8ad911ee11869
Paramprakash Makwana <prakashmakwana7808@gmail.com>
2026-06-29 16:40:04 +0530

    commit

--- Assignment/Module 13) Python Fundamentals/Lab/5.3.py
+++ Assignment/Module 13) Python Fundamentals/Lab/5.3.py
@@ -5,3 +5,10 @@ List1 = ['apple', 'banana', 'mango']
 for i in List1:
     if i == "banana":
         print(i)
+
+# l1 = input("Enter Fruits:").split()
+# f1 = input("Enter Fruits to Find:")
+
+# for i in l1:
+#     if i == f1:
+#         print(i)
